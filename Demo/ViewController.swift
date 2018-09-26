aa93b5f358cb348f3f6a66bbceb34ce0d6111ea0
Jett Farmer <jettfdev@gmail.com>
2018-09-26 12:05:38 -0700

    Example project now dismisses the picker on cancel or done. Minor formatting updates.

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -11,29 +11,30 @@ class ViewController: UIViewController, ContactsPickerDelegate {
     let contactPickerScene = ContactsPicker(delegate: self, multiSelection: true, subtitleCellType: SubtitleCellValue.email)
     let navigationController = UINavigationController(rootViewController: contactPickerScene)
     self.present(navigationController, animated: true, completion: nil)
-    
   }
     
 //MARK: EPContactsPicker delegates
-    func contactPicker(_: ContactsPicker, didContactFetchFailed error: NSError)
-    {
+    func contactPicker(_: ContactsPicker, didContactFetchFailed error: NSError) {
         print("Failed with error \(error.description)")
     }
     
-    func contactPicker(_: ContactsPicker, didSelectContact contact: Contact)
-    {
+    func contactPicker(_: ContactsPicker, didSelectContact contact: Contact) {
         print("Contact \(contact.displayName) has been selected")
     }
     
-    func contactPickerDidCancel(_: ContactsPicker) {
+    func contactPickerDidCancel(_ picker: ContactsPicker) {
+        picker.dismiss(animated: true, completion: nil)
         print("User canceled the selection");
     }
     
-    func contactPicker(_: ContactsPicker, didSelectMultipleContacts contacts: [Contact]) {
+    func contactPicker(_ picker: ContactsPicker, didSelectMultipleContacts contacts: [Contact]) {
+        defer { picker.dismiss(animated: true, completion: nil) }
+        guard !contacts.isEmpty else { return }
         print("The following contacts are selected")
         for contact in contacts {
             print("\(contact.displayName)")
         }
+    
     }
 
 }
